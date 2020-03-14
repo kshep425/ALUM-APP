549fcba1291288a3d6ad1f3a5581ad1f9e9fcdcc
Keisha <kshep425@gmail.com>
2020-03-14 04:50:03 -0400

    Added sign out form

--- frontend/src/components/SignOut/index.js
+++ frontend/src/components/SignOut/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import {withFirebase} from '../Firebase';
 
-const SignOut = () => {
-  return (
-    <div>
-      <h1>SignOut</h1>
-    </div>
-  );
-};
+const SignOutButton = ({firebase}) => (
+  <button type="button" onClick={firebase.doSignOut}>
+    Sign Out
+  </button>
+);
 
-export default SignOut;
+export default withFirebase(SignOutButton);
