37c939651459ad81efe8e88ec7216e667425ccda
Keisha <kshep425@gmail.com>
2020-03-14 15:57:02 -0400

    Added password change form

--- frontend/src/components/Account/index.js
+++ frontend/src/components/Account/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Account = () => {
-  return (
-    <div>
-      <h1>Account</h1>
-    </div>
-  );
-};
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
 
-export default Account;
+const AccountPage = () => (
+  <div>
+    <h1>Account Page</h1>
+    <PasswordForgetForm />
+    <PasswordChangeForm />
+  </div>
+);
+
+export default AccountPage;
