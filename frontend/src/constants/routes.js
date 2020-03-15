7fc461611444a03c102e198b9b10786b20ea6f80
Keisha <kshep425@gmail.com>
2020-03-15 15:42:09 -0400

    Added more routes

--- frontend/src/constants/routes.js
+++ frontend/src/constants/routes.js
@@ -1,8 +1,13 @@
 export const LANDING = '/';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
-export const HOME = '/home';
-export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
+export const HOME = '/home';
+export const MYMSU = '/mymsu';
+export const ACCOUNTS = '/accounts';
+export const ACCOUNT_DETAILS = '/accounts/:id';
+export const EVENTS = '/events';
+export const EVENT_DETAILS = '/events/:id';
 export const ADMIN = '/admin';
 export const ADMIN_DETAILS = '/admin/:id';
+export const DONATE = '/donate';
