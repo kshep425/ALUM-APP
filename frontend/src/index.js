e6fdc98f38d294bff166f289445c8e95adf906a7
Keisha <kshep425@gmail.com>
2020-03-15 13:26:22 -0400

    Just adding back the currect ./App path that we will use when this gets merged in to the master.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+// import App from './App';
 import Firebase, { FirebaseContext } from './components/Firebase';
 import * as serviceWorker from './serviceWorker';
 
