61bc64870b083f9cc70333e717cc99260f9996a9
Keisha <kshep425@gmail.com>
2020-03-15 16:15:52 -0400

    Went back to using the original App file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
-// import App from './App';
+import App from './App';
 import Firebase, { FirebaseContext } from './components/Firebase';
 import * as serviceWorker from './serviceWorker';
 
