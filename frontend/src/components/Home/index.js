e15a2e83cecde5f2637a810244553b21f3f41cca
Keisha <kshep425@gmail.com>
2020-03-15 15:39:51 -0400

    Added display name to page

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { compose } from 'recompose';
 
-import { withAuthorization, withEmailVerification } from '../Session';
+import { AuthUserContext, withAuthorization, withEmailVerification } from '../Session';
 import Messages from '../Messages';
 
 const HomePage = () => (
-
+<AuthUserContext.Consumer>
+    {authUser => (
   <div>
     <h1>Home Page</h1>
+    <h4>Welcome {authUser.providerData[0].displayName} </h4>
+    
     <p>The Home Page is accessible by every signed in user.</p>
 
     <Messages />
   </div>
+    )}
+    </AuthUserContext.Consumer>
 );
 
 // conditional statement to see if the user is authorized or not. authUser returns a user if logged in, and returns null if the user is not logged in.
