941aa99bdc3d8493541446e5b75b5949018727c1
Keisha <kshep425@gmail.com>
2020-03-19 22:10:47 -0400

    Chnaged a to Link, fixed how it works

--- frontend/src/building_components/Navbar/NavItem.js
+++ frontend/src/building_components/Navbar/NavItem.js
@@ -1,14 +1,13 @@
 import React from "react";
-import Link from "react-router-dom"
+import { Link } from "react-router-dom"
 import "./style.css";
 
 const Navitem = props => {
-  // console.log(props);
   return (
-    <Link to={props.href} className="navLink">
-      <div className="navItemDiv">
-        <i className="fa fa-caret-right"> </i>
-          {props.children}
+    <Link to={props.href} className="navItemDiv">
+      <div className="navLink">
+        <i className="fa fa-caret-right"></i>
+        {props.children}
       </div>
     </Link>
   );
