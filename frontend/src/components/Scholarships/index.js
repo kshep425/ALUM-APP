8261af153e732b6a02f76f31e9324ccfa011298c
Jody Jones <jodybjones@yahoo.com>
2020-03-19 22:03:13 -0400

    started editing the page. need to add pdf scholarship form

--- frontend/src/components/Scholarships/index.js
+++ frontend/src/components/Scholarships/index.js
@@ -3,7 +3,57 @@ import React from 'react';
 const Scholarships = () => {
   return (
     <div>
-      <h1>Scholarship info</h1>
+      <h1>scholarship Stuff will go HERE DUDE</h1>
+      <h2>incoming Scholarship document here</h2>
+      <h2>Howard County Chapter Bear Pride Scholarship Application</h2>
+      <p>For New Students
+
+      The Howard County Chapter of Morgan State University Alumni Association is offering scholarship awards to Howard County residents who are graduating Howard County High School seniors or currently attending Howard Community College. The applicant must plan to enroll and attend Morgan State University for the upcoming academic year.
+
+</p>
+      <h2>Eligibility:</h2>
+      <br></br>
+      <p>To qualify for this scholarship, the applicant must:
+
+<li>Be a Howard County High School graduating senior or a student transferring to Morgan from Howard Community College.</li>
+        <li>Be accepted at Morgan State University.</li>
+        <li>Be a Howard County resident.</li>
+        <li>Have a combined math, writing and reading SAT score of 1200 or better.</li>
+
+      </p>
+      <h2>Selection:</h2>
+      <br></br>
+      <p>Scholarship applications are to be postmarked no later than April 1st. Scholarship recipients will be determined no later than April 15th by the chapter scholarship committee. Recipients will be notified by April 30th.  If the scholarship applicant does not receive an award notification by April 30th, it unfortunately means that the applicant will not receive a scholarship award for this application year.
+</p>
+
+      <h2>Awards:</h2>
+      <br></br>
+      <p>Awards vary in amount. The amount of the award will be announced and presented at the Howard County Chapter Scholarship Awards Ceremony in June. Checks will be disbursed upon verification of student enrollment as a full- time student at Morgan State University.
+</p>
+
+<h2>The Application Package:</h2>
+<br></br>
+<p>Consideration will be given to candidates submitting complete application packages. To apply follow these steps:
+<br></br>
+<li>Print or type all required information on the attached application.</li>
+<li>Attach an official, sealed transcript containing current grades.</li>
+<li>Attach a copy of combined SAT score (applicable to Howard County High School students).</li>
+<li>Attach two sealed letters of recommendation from non-family members (school, community, church).</li>
+<li>Attach a 500-word essay which clearly explains how this scholarship will assist you in achieving your goal of obtaining a college education at Morgan State University.</li>
+<br></br>
+A complete application package must be postmarked no later than April 1st of the application year.
+<br></br>
+<h4>Mail to:</h4>
+<ul>Scholarship Committee </ul>
+<ul>Howard County Chapter</ul>
+<ul>Morgan State University</ul>
+<ul>Alumni Association</ul>
+<ul>P.O. Box 6847</ul>
+<ul>Columbia, Maryland 21045</ul>
+
+
+</p>
+
     </div>
   );
 };
