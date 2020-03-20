a289ffd67c4af263ec1b47a85c4f506f75709b5c
Keisha <kshep425@gmail.com>
2020-03-19 23:02:12 -0400

    Replacing a tag with link and surrounding the link with the logo and the text

--- frontend/src/building_components/Logo/index.js
+++ frontend/src/building_components/Logo/index.js
@@ -1,19 +1,20 @@
 
 import React from 'react';
 import logo from "../../images/MSULNO1C.svg";
+import { Link } from "react-router-dom"
 const Logo = () => {
   return (
-  <div className="lowerNav">
-    <div className="row logoDiv">
-      <div className="col">
-        <a href="/">
-          <img src={logo} alt="MSU Logo" className="logo"></img>
-        </a>
-        <h2 className="alumniText">ALUMNI</h2>
+    <div className="lowerNav">
+      <div className="row logoDiv">
+        <div className="col">
+          <Link to="/">
+            <img src={logo} alt="MSU Logo" className="logo"></img>
+            <h2 className="alumniText">ALUMNI</h2>
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
-)
-  }
+  )
+}
 
 export default Logo;
