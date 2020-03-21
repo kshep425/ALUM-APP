f32a08ed551ee06df896131fec6732963975f79e
Keisha <kshep425@gmail.com>
2020-03-21 19:44:03 -0400

    Added events link

--- Backend/models/member.js
+++ Backend/models/member.js
@@ -59,6 +59,10 @@ module.exports = function (sequelize, DataTypes) {
         models.Member.hasMany(models.Payment);
     }
 
+    Member.associate = function (models) {
+      models.Member.hasMany(models.Event);
+  }
+
     // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
     Member.prototype.validPassword = function (password) {
         return bcrypt.compareSync(password, this.password);
