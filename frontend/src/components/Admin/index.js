4403f18b67dbfa5a2ee0a4825878e3b281a9def0
Keisha <kshep425@gmail.com>
2020-03-21 20:12:53 -0400

    Adding more to the admin page

--- frontend/src/components/Admin/index.js
+++ frontend/src/components/Admin/index.js
@@ -2,21 +2,39 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { compose } from 'recompose';
 
-import { withAuthorization, withEmailVerification } from '../Session';
+import { withAuthUserContext, withAuthorization, withEmailVerification, AuthUserContext } from '../Session';
 import { UserList, UserItem } from '../Users';
 import * as ROLES from '../../constants/roles';
 import * as ROUTES from '../../constants/routes';
 
+
 const AdminPage = () => (
-  <div>
+  <AuthUserContext.Consumer>
+    {
+      authUser =>
+      <div>
     <h1>Admin</h1>
     <p>The Admin Page is accessible by every signed in admin user.</p>
+    <p>Current user role:</p>
+      {/* <p>Admin: {authUser.roles.ADMIN}</p>
+      <p>BOARD: {authUser.roles.BOARD}</p>
+      <p>User: {authUser.roles.USER}</p>
+      {authUser.roles.map((role) => <p key={authUser.id}>{role}</p>)} */}
 
+    <p>Select User Role</p>
+    <select>
+      <option value={ROLES.ADMIN}>{ROLES.ADMIN}</option>
+      <option value={ROLES.BOARD}>{ROLES.BOARD}</option>
+      <option value={ROLES.USER}>{ROLES.USER}</option>
+    </select>
+    <input></input>
     <Switch>
       <Route exact path={ROUTES.ADMIN_DETAILS} component={UserItem} />
       <Route exact path={ROUTES.ADMIN} component={UserList} />
     </Switch>
   </div>
+    }
+  </AuthUserContext.Consumer>
 );
 
 const condition = authUser =>
