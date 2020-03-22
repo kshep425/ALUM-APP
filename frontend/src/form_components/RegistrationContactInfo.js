08d7d377e5adeee60e6d61dde4b70463fae1cb26
Keisha <kshep425@gmail.com>
2020-03-22 02:36:18 -0400

    Adding link to edit the page and moving contact info into an address card; still needs better design

--- frontend/src/form_components/RegistrationContactInfo.js
+++ frontend/src/form_components/RegistrationContactInfo.js
@@ -1,109 +1,125 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { AuthUserContext } from '../components/Session';
+import get from 'lodash/get'
 
-class RegistrionContactInfo extends Component {
+function RegistrationContactInfo(props) {
 
-    constructor(props) {
-        super(props)
 
-        this.state = {
-
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <div class="form-group">
-                    <label for="full_name">Full Name</label>
-                    <input id="full_name" type="text" class="form-control"></input>
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email" type="email" autocomplete="username" class="form-control"></input>
-                </div>
-                <div class="form-group">
-                    <label for="inputPhone">Phone Number</label>
-                    <input type="text" class="form-control" id="inputPhone" placeholder="Phone Number"></input>
-                </div>
-                <div class="form-group">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"></input>
-                </div>
-                <div class="form-group">
-                    <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2"
-                        placeholder="Apartment, studio, or floor"></input>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity"></input>
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="inputState">State</label>
-                        <select id="inputState" class="form-control">
-                            <option selected>Choose...</option>
-                            <option value="AL">Alabama (AL)</option>
-                            <option value="AK">Alaska (AK)</option>
-                            <option value="AZ">Arizona (AZ)</option>
-                            <option value="AR">Arkansas (AR)</option>
-                            <option value="CA">California (CA)</option>
-                            <option value="CO">Colorado (CO)</option>
-                            <option value="CT">Connecticut (CT)</option>
-                            <option value="DE">Delaware (DE)</option>
-                            <option value="DC">District Of Columbia (DC)</option>
-                            <option value="FL">Florida (FL)</option>
-                            <option value="GA">Georgia (GA)</option>
-                            <option value="HI">Hawaii (HI)</option>
-                            <option value="ID">Idaho (ID)</option>
-                            <option value="IL">Illinois (IL)</option>
-                            <option value="IN">Indiana (IN)</option>
-                            <option value="IA">Iowa (IA)</option>
-                            <option value="KS">Kansas (KS)</option>
-                            <option value="KY">Kentucky (KY)</option>
-                            <option value="LA">Louisiana (LA)</option>
-                            <option value="ME">Maine (ME)</option>
-                            <option value="MD">Maryland (MD)</option>
-                            <option value="MA">Massachusetts (MA)</option>
-                            <option value="MI">Michigan (MI)</option>
-                            <option value="MN">Minnesota (MN)</option>
-                            <option value="MS">Mississippi (MS)</option>
-                            <option value="MO">Missouri (MO)</option>
-                            <option value="MT">Montana (MT)</option>
-                            <option value="NE">Nebraska (NE)</option>
-                            <option value="NV">Nevada (NV)</option>
-                            <option value="NH">New Hampshire (NH)</option>
-                            <option value="NJ">New Jersey (NJ)</option>
-                            <option value="NM">New Mexico (NM)</option>
-                            <option value="NY">New York (NY)</option>
-                            <option value="NC">North Carolina (NC)</option>
-                            <option value="ND">North Dakota (ND)</option>
-                            <option value="OH">Ohio (OH)</option>
-                            <option value="OK">Oklahoma (OK)</option>
-                            <option value="OR">Oregon (OR)</option>
-                            <option value="PA">Pennsylvania (PA)</option>
-                            <option value="RI">Rhode Island (RI)</option>
-                            <option value="SC">South Carolina (SC)</option>
-                            <option value="SD">South Dakota (SD)</option>
-                            <option value="TN">Tennessee (TN)</option>
-                            <option value="TX">Texas (TX)</option>
-                            <option value="UT">Utah (UT)</option>
-                            <option value="VT">Vermont (VT)</option>
-                            <option value="VA">Virginia (VA)</option>
-                            <option value="WA">Washington (WA)</option>
-                            <option value="WV">West Virginia (WV)</option>
-                            <option value="WI">Wisconsin (WI)</option>
-                            <option value="WY">Wyoming (WY)</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-2">
-                        <label for="inputZip">Zip</label>
-                        <input type="text" class="form-control" id="inputZip"></input>
-                    </div>
-                </div>
+  return (
+    <AuthUserContext.Consumer>
+      {authUser =>
+        (
+          <div className="container">
+            <div className="form-group">
+              <label htmlFor="uid">uid</label>
+              <input uid="uid" type="text" className="form-control" defaultValue={get(authUser, "uid")} ref={props.uidRef}></input>
             </div>
-        )
-    }
+            <div className="form-group">
+              <label htmlFor="prefix">Prefix</label>
+              <input id="prefix" type="text" className="form-control" defaultValue={get(authUser, 'db.prefix')} ref={props.prefixRef}></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor="firstName">First Name</label>
+              <input id="firstName" type="text" className="form-control" defaultValue={get(authUser, "db.firstName")} ref={props.firstNameRef}></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastName">Last Name</label>
+              <input id="lastName" type="text" className="form-control" defaultValue={get(authUser, "db.lastName")} ref={props.lastNameRef}></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor="suffix">Suffix</label>
+              <input id="suffix" type="text" className="form-control" defaultValue={get(authUser, "db.suffix")} ref={props.suffixRef}></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input id="email" type="email" autoComplete="username" className="form-control" defaultValue={get(authUser, "db.email")} ref={props.emailRef}></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputPhone">Phone Number</label>
+              <input type="text" className="form-control" id="inputPhone" defaultValue={get(authUser, "db.phone")} ref={props.phoneRef}></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputAddress">Address</label>
+              <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" defaultValue={get(authUser, "db.streetAddress1")} ref={props.streetAddress1Ref}></input>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputAddress2">Address 2</label>
+              <input type="text" className="form-control" id="inputAddress2"
+                placeholder="Apartment, studio, or floor" defaultValue={get(authUser, "db.streetAddress2")} ref={props.streetAddress2Ref}></input>
+            </div>
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label htmlFor="inputCity">City</label>
+                <input type="text" className="form-control" id="inputCity" defaultValue={get(authUser, "db.city")} ref={props.cityRef}></input>
+              </div>
+              <div className="form-group col-md-4">
+                <label htmlFor="inputState">State</label>
+                <select id="inputState" className="form-control" defaultValue={get(authUser, "db.state")} ref={props.stateRef}>
+                  <option>Choose...</option>
+                  <option defaultValue="AL">Alabama (AL)</option>
+                  <option defaultValue="AK">Alaska (AK)</option>
+                  <option defaultValue="AZ">Arizona (AZ)</option>
+                  <option defaultValue="AR">Arkansas (AR)</option>
+                  <option defaultValue="CA">California (CA)</option>
+                  <option defaultValue="CO">Colorado (CO)</option>
+                  <option defaultValue="CT">Connecticut (CT)</option>
+                  <option defaultValue="DE">Delaware (DE)</option>
+                  <option defaultValue="DC">District Of Columbia (DC)</option>
+                  <option defaultValue="FL">Florida (FL)</option>
+                  <option defaultValue="GA">Georgia (GA)</option>
+                  <option defaultValue="HI">Hawaii (HI)</option>
+                  <option defaultValue="ID">Idaho (ID)</option>
+                  <option defaultValue="IL">Illinois (IL)</option>
+                  <option defaultValue="IN">Indiana (IN)</option>
+                  <option defaultValue="IA">Iowa (IA)</option>
+                  <option defaultValue="KS">Kansas (KS)</option>
+                  <option defaultValue="KY">Kentucky (KY)</option>
+                  <option defaultValue="LA">Louisiana (LA)</option>
+                  <option defaultValue="ME">Maine (ME)</option>
+                  <option defaultValue="MD">Maryland (MD)</option>
+                  <option defaultValue="MA">Massachusetts (MA)</option>
+                  <option defaultValue="MI">Michigan (MI)</option>
+                  <option defaultValue="MN">Minnesota (MN)</option>
+                  <option defaultValue="MS">Mississippi (MS)</option>
+                  <option defaultValue="MO">Missouri (MO)</option>
+                  <option defaultValue="MT">Montana (MT)</option>
+                  <option defaultValue="NE">Nebraska (NE)</option>
+                  <option defaultValue="NV">Nevada (NV)</option>
+                  <option defaultValue="NH">New Hampshire (NH)</option>
+                  <option defaultValue="NJ">New Jersey (NJ)</option>
+                  <option defaultValue="NM">New Mexico (NM)</option>
+                  <option defaultValue="NY">New York (NY)</option>
+                  <option defaultValue="NC">North Carolina (NC)</option>
+                  <option defaultValue="ND">North Dakota (ND)</option>
+                  <option defaultValue="OH">Ohio (OH)</option>
+                  <option defaultValue="OK">Oklahoma (OK)</option>
+                  <option defaultValue="OR">Oregon (OR)</option>
+                  <option defaultValue="PA">Pennsylvania (PA)</option>
+                  <option defaultValue="RI">Rhode Island (RI)</option>
+                  <option defaultValue="SC">South Carolina (SC)</option>
+                  <option defaultValue="SD">South Dakota (SD)</option>
+                  <option defaultValue="TN">Tennessee (TN)</option>
+                  <option defaultValue="TX">Texas (TX)</option>
+                  <option defaultValue="UT">Utah (UT)</option>
+                  <option defaultValue="VT">Vermont (VT)</option>
+                  <option defaultValue="VA">Virginia (VA)</option>
+                  <option defaultValue="WA">Washington (WA)</option>
+                  <option defaultValue="WV">West Virginia (WV)</option>
+                  <option defaultValue="WI">Wisconsin (WI)</option>
+                  <option defaultValue="WY">Wyoming (WY)</option>
+                </select>
+              </div>
+              <div className="form-group col-md-2">
+                <label htmlFor="inputZip">Zip</label>
+                <input type="text" className="form-control" id="inputZip" defaultValue={get(authUser, "db.zip")} ref={props.zipRef}></input>
+              </div>
+            </div>
+          </div>
+        )}
+
+    </AuthUserContext.Consumer>
+  )
+
 }
 
-export default RegistrionContactInfo;
+export default RegistrationContactInfo;
