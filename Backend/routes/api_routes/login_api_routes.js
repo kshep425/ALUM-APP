ac8904d9d34c2e8ec054a16d6776842c1cacb048
Keisha <kshep425@gmail.com>
2020-03-23 23:08:24 -0400

    Removing references to passport will eventually remove all files related to that

--- Backend/routes/api_routes/login_api_routes.js
+++ Backend/routes/api_routes/login_api_routes.js
@@ -1,19 +1,17 @@
 const db_queries = require("../../config/db_queries");
-const passport = require("../../config/passport");
-
 // Using the passport.authenticate middleware with our local strategy.
 // If the user has valid login credentials, send them to the members page.
 // Otherwise the user will be sent an error
 module.exports = function(app) {
   //
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    console.log(req.body);
-    //console.log("Login Registered User: " + req.user.username)
-    res
-      .status(200)
-      // .json({ username: req.user.username, full_name: req.user.full_name });
-      .json(req);
-  });
+  // app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  //   console.log(req.body);
+  //   //console.log("Login Registered User: " + req.user.username)
+  //   res
+  //     .status(200)
+  //     // .json({ username: req.user.username, full_name: req.user.full_name });
+  //     .json(req);
+  // });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
