da3032c8877bcb13b59b527dc194d9c85f7532dd
Jody Jones <jodybjones@yahoo.com>
2020-04-01 17:58:17 -0400

    adding card details

---
+++ frontend/src/building_components/Card/NewCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./card.css";
+import Memeber from "../../building_components/member.json"
+
+function NewCard(props) {
+
+
+  return (
+    
+
+
+    // <div className="card">
+    //   <div className="img-container">
+    //     {/* <img alt={props.name} src={props.image} /> */}
+    //   </div>
+    //   <div className="content">
+    //     <ul>
+    //       <li>
+    //         <strong>{props.name}</strong> 
+    //       </li>
+    //       <li>
+    //         <strong>{props.position}</strong> 
+    //       </li>
+    //       <li>
+    //         <strong>About:</strong> {props.about}
+    //       </li>
+    //     </ul>
+    //   </div>
+    
+    // </div>
+
+<div className="card">
+
+<div className="card-body" style={{float:"left"}}>
+  <h5 className="card-title">{props.name}</h5>
+  <p className="card-subtitle">{props.title}</p>
+  <p className="card-stext">{props.position}</p>
+</div>
+</div>
+  );
+}
+
+export default NewCard;
