910d9bd2e0dd57a7a65114d784d27c895e91ab4a
Keisha <kshep425@gmail.com>
2020-04-01 19:39:17 -0400

    Updated Navbars and logo images.
    Made navbar more horizontal and  media query friendly;
    Updated favicon;

--- frontend/src/building_components/Logo/index.js
+++ frontend/src/building_components/Logo/index.js
@@ -1,18 +1,15 @@
 
 import React from 'react';
-import logo from "../../images/MSULNO1C.svg";
+import logo from "../../images/MSU Howard County Alumni Chapter logo_NEW.png";
 import { Link } from "react-router-dom"
 import {HOME} from "../../constants/routes"
 const Logo = () => {
   return (
     <div className="lowerNav">
-      <div className="row logoDiv">
-        <div className="col">
+      <div className="logoDiv">
           <Link to={HOME}>
             <img src={logo} alt="MSU Logo" className="logo"></img>
-            <h2 className="alumniText">ALUMNI</h2>
           </Link>
-        </div>
       </div>
     </div>
   )
