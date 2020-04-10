58090343b350ff60f86883976309a1bcfeaf6776
Keisha <kshep425@gmail.com>
2020-04-09 21:38:51 -0400

    Home Page refactor Added background fixed issue with carousel height going everwhere on small screens
    Added background fixed issue with carousel height going everwhere on small screens

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Carousel from "../../building_components/Carousel/Carousel";
+import './style.css'
+
+const HomePage = () => (
+  <div className="mainPage divPage">
+    <Carousel />
+  </div>
+);
+
+export default HomePage;
