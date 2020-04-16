7019958def9aa8a51d656cb2bfed9bbf24282767
Keisha <kshep425@gmail.com>
2020-04-16 09:33:15 -0400

    Updated style for Scholarships Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import HomePage from "./components/Home";
 import MyMsuPage from "./components/MyMSU";
 import EventsPage from "./components/Events";
 import DonatePage from "./components/Donate";
-import ShcolarshipPage from "./components/Scholarships";
+import ScholarshipPage from "./components/Scholarships";
 import About from "./components/About";
 import AdminPage from "./components/Admin";
 import EditContactInfo from "./components/MyMSU/EditContactInfo";
@@ -34,7 +34,7 @@ class App extends Component {
             <Route path={ROUTES.EVENTS} component={EventsPage} />
             <Route path={ROUTES.ADMIN} component={AdminPage} />
             <Route path={ROUTES.DONATE} component={DonatePage} />
-            <Route path={ROUTES.SCHOLARSHIPS} component={ShcolarshipPage} />
+            <Route path={ROUTES.SCHOLARSHIPS} component={ScholarshipPage} />
             <Route path={ROUTES.ABOUT} component={About} />
             <Route path={ROUTES.EDIT} component={EditContactInfo} />
             <Route path={ROUTES.EDIT_MEMBER_INFO} component={EditMemberInfo} />
